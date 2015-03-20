6a6c8e1e5e93dfca0b16c7ba375460d2f2087d25
Gilberto <gilberto.aristizabal@mail.utoronto.ca>
2015-03-19 22:30:59 -0400

    started a new app

---
+++ starUpIdeas/app/controllers/ideas_controller.rb
@@ -0,0 +1,51 @@
+class IdeasController < ApplicationController
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+	def index
+    	@ideas = Idea.all
+  	end
+
+	def show
+    	@idea = Idea.find(params[:id])
+  	end
+
+	def new
+		@idea = Idea.new
+	end
+
+	def edit
+  		@idea = Idea.find(params[:id])
+	end
+
+	def create
+	  @idea = Idea.new(idea_params)
+	 
+		if @idea.save
+    		redirect_to @idea # redirect_to: tells the browser to issue another request.
+  		else
+    		render 'new' # same request is used as the form submission
+  		end
+	end
+
+	def update
+	  @idea = Idea.find(params[:id])
+	 
+	  if @idea.update(idea_params)
+	    redirect_to @idea
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+	  @idea = Idea.find(params[:id])
+	  @idea.destroy
+	 
+	  redirect_to ideas_path
+	end
+	 
+	private
+	def idea_params
+	    params.require(:idea).permit(:title, :description)
+  	end
+end
